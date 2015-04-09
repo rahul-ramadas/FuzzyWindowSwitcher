e841e83d4039f331ed6815d75fb1d5d911f20f8d
Rahul Ramadas <tripshock@gmail.com>
2015-04-08 21:46:56 -0700

    Basic window enumeration.

--- FuzzyWindowSwitcher/MainWindow.xaml.cs
+++ FuzzyWindowSwitcher/MainWindow.xaml.cs
@@ -12,17 +12,75 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Drawing;
+using System.IO;
+using ManagedWinapi.Windows;
 
 namespace FuzzyWindowSwitcher
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            m_NotifyIcon = new System.Windows.Forms.NotifyIcon();
+            Stream iconStream = System.Windows.Application.GetResourceStream(new Uri("pack://application:,,,/FuzzyWindowSwitcher.ico")).Stream;
+            m_NotifyIcon.Icon = new System.Drawing.Icon(iconStream);
+            iconStream.Dispose();
+            m_NotifyIcon.Visible = true;
+            m_NotifyIcon.Click +=
+                delegate(object Sender, EventArgs Args)
+                {
+                    this.Show();
+                    this.WindowState = WindowState.Normal;
+                };
+
+            this.PreviewKeyDown += new KeyEventHandler(HandleEsc);
+
+            var windows = SystemWindow.FilterToplevelWindows(new Predicate<SystemWindow>(IsAppWindow));
+
+            m_Windows = new List<string>();
+            foreach (var window in windows)
+            {
+                m_Windows.Add(window.Title);
+            }
+
+            WindowList.ItemsSource = m_Windows;
         }
+
+        static bool IsAppWindow(SystemWindow Window)
+        {
+            return Window.Visible &&
+                !Window.ExtendedStyle.HasFlag(WindowExStyleFlags.TOOLWINDOW);
+        }
+
+        private void HandleEsc(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                Close();
+            }
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            m_NotifyIcon.Visible = false;
+
+            base.OnClosed(e);
+        }
+
+        protected override void OnStateChanged(EventArgs e)
+        {
+            if (WindowState == WindowState.Minimized)
+            {
+                this.Hide();
+            }
+
+            base.OnStateChanged(e);
+        }
+
+        private System.Windows.Forms.NotifyIcon m_NotifyIcon;
+        private List<string> m_Windows;
     }
 }
